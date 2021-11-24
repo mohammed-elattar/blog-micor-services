bdb5ac3ae0371fb95aa4a5ac998972975bcb9e99
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-24 02:07:27 +0100

    add comments apis

---
+++ comments/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const randomBytes = require('random-bytes');
+const app = express();
+const port = 3011;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const commentsByPostId = [];
+
+app.get('/posts/:id/comments', (req, res) => {
+  return res.json(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const random = await randomBytes(4);
+  const id = random.toString('hex');
+  const postComments = commentsByPostId[req.params.id] || [];
+  const content = req.body.content;
+  postComments.push({ id, content });
+  commentsByPostId[req.params.id] = postComments;
+
+  return res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
